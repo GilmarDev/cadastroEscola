47943e323123916700edd8c827aefac26e52467b
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-23 16:33:55 -0300

    Criado metodo Post estudante

--- src/main/java/com/escolaadventista/cadastro/controller/EstudanteController.java
+++ src/main/java/com/escolaadventista/cadastro/controller/EstudanteController.java
@@ -3,8 +3,12 @@ package com.escolaadventista.cadastro.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.escolaadventista.cadastro.model.Estudante;
@@ -25,4 +29,9 @@ public class EstudanteController {
 		return estudanteRepository.findAll();
 	}
 	
+	@PostMapping("/estudante")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Estudante adicionarAluno(@RequestBody Estudante estudante) {
+		return estudanteRepository.save(estudante);
+	}
 }
