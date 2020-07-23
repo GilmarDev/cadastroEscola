92b515bfd6657362d0db5f0821a857a95009e00c
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-23 18:02:43 -0300

    Criado metodo Delete para estudante

--- src/main/java/com/escolaadventista/cadastro/controller/EstudanteController.java
+++ src/main/java/com/escolaadventista/cadastro/controller/EstudanteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,4 +47,19 @@ public class EstudanteController {
 		estudante = estudanteRepository.save(estudante);
 		return ResponseEntity.ok(estudante);
 	}
+	
+	
+	@DeleteMapping("/estudante/{estudanteId}")
+	public ResponseEntity<Void> removerAluno(@PathVariable Long estudanteId){
+		
+		if(!estudanteRepository.existsById(estudanteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		estudanteRepository.deleteById(estudanteId);
+		return ResponseEntity.noContent().build();
+				
+	
+	}
+	
+	
 }
